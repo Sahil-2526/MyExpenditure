500331ec0c1fe6754a3e0799246bfad01a572a3b
Sahil-2526 <sahil.26.02.2007@gmail.com>
2026-07-01 23:39:14 +0530

    category wise spending

--- finance.py
+++ finance.py
@@ -2,12 +2,12 @@ from model import Transaction
 transactions = []
 
 # Add transaction 
-def addTransac(date, amount, type, category, note):
+def add_transaction(date, amount, type, category, note):
     transac = Transaction(date, amount, type, category, note)
     transactions.append(transac)
 
 # View all transaction
-def viewTransac():
+def view_transaction():
     return transactions
 
 # Calculate total_credit
@@ -17,3 +17,15 @@ def total_credit():
 # Calculate total_debit
 def total_debit():
     return sum(t.amount for t in transactions if t.type == "Debit")
+
+# calculate balance
+def balance():
+    return total_credit()-total_debit()
+
+# category wise spending
+def category_wise_spending():
+    summary = {}
+    for t in transactions:
+        if t.type == "Debit":
+            summary[t.category] = summary.get(t.category, 0) + t.amount
+    return summary
