edf32434eb2d6fd34cdde3b9bb5d619a1130f2ab
Sahil-2526 <sahil.26.02.2007@gmail.com>
2026-07-03 14:51:50 +0530

    Testing category class

---
+++ category_test.py
@@ -0,0 +1,9 @@
+from category import Category
+
+food = Category("Food", "Expense", True)
+salary = Category("Salary", "Income", True)
+gaming = Category("Gaming", "Expense")
+
+print(food)
+print(salary)
+print(gaming)
