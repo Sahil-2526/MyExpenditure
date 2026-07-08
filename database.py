0182d2339573704482ca19ebeb5482b086c1b016
Sahil-2526 <sahil.26.02.2007@gmail.com>
2026-07-08 23:31:08 +0530

    tranaction table created

--- database.py
+++ database.py
@@ -5,3 +5,17 @@ class Database:
         self.connection = sqlite3.connect(db_name)
         self.cursor = self.connection.cursor()
 
+    def create_tables(self):
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS transactions (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                amount REAL NOT NULL,
+                date TEXT NOT NULL,
+                transaction_type TEXT NOT NULL,
+                category_id INTEGER NOT NULL,
+                note TEXT,
+                FOREIGN KEY(category_id) REFERENCES categories(id)
+            )
+        """)
+                
+        self.connection.commit()
