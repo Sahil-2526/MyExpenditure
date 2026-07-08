49a23e0f5437398bf9128c1a568c4e6ce490c73a
Sahil-2526 <sahil.26.02.2007@gmail.com>
2026-07-08 23:43:19 +0530

    budget, category, goal table added

--- database.py
+++ database.py
@@ -17,5 +17,34 @@ class Database:
                 FOREIGN KEY(category_id) REFERENCES categories(id)
             )
         """)
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS categories (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE,
+                transaction_type TEXT NOT NULL,
+                is_default INTEGER NOT NULL
+            )
+        """)
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS budgets (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                category_id INTEGER NOT NULL,
+                limit_amount REAL NOT NULL,
+                month INTEGER NOT NULL,
+                year INTEGER NOT NULL,
+                FOREIGN KEY(category_id) REFERENCES categories(id)
+            )
+        """)
+    
+        self.cursor.execute("""
+            CREATE TAVLE IF NOT EXISTS goals (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE,
+                target_amount REAL NOT NULL,
+                deadline TEXT
+            )
+        """)
                 
         self.connection.commit()
